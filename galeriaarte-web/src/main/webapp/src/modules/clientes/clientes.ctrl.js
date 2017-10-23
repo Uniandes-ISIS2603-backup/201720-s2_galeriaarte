d605d293b81b16d100e88a5997987684fa80f753
Kelvin <ks.estupinan@uniandes.edu.co>
2017-10-23 13:12:26 -0500

    Correcion Modulos

--- galeriaarte-web/src/main/webapp/src/modules/clientes/clientes.ctrl.js
+++ galeriaarte-web/src/main/webapp/src/modules/clientes/clientes.ctrl.js
@@ -1,13 +1,12 @@
 (function (ng) {
-    var mod = ng.module("editorialModule");
-    mod.constant("editorialContext", "api/editorials");
-    mod.controller('editorialCtrl', ['$scope', '$http', 'editorialContext',
+    var mod = ng.module("clienteModule");
+    mod.constant("clienteContext", "api/clientes");
+    mod.controller('clienteCtrl', ['$scope', '$http', 'clienteContext',
         function ($scope, $http, editorialContext) {
-            //'data/clientes.json'  la ubicacion donde esta el archivo de pruebas
             $http.get('data/clientes.json').then(function (response) {
-                $scope.editorialsRecords = response.data;
+                $scope.clientesRecords = response.data;
             });
         }
     ]);
 }
-)(windows.angular);
+)(angular);
