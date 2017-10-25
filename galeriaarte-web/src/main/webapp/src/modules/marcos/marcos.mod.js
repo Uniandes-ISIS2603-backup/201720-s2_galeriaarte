756ad0614636978182da068541f75860bed61841
Andres Leon <af.leon@uniandes.edu.co>
2017-10-24 20:48:19 -0500

    Mejoras Marco

--- galeriaarte-web/src/main/webapp/src/modules/marcos/marcos.mod.js
+++ galeriaarte-web/src/main/webapp/src/modules/marcos/marcos.mod.js
@@ -1,27 +1,47 @@
 (function (ng) {
-    // Definición del módulo
     var mod = ng.module("marcoModule", ['ui.router']);
-
-    // Configuración de los estados del módulo
+    mod.constant("marcosContext", "api/marcos");
     mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-            // En basePath se encuentran los templates y controladores de módulo
             var basePath = 'src/modules/marcos/';
-            // Mostrar la lista de libros será el estado por defecto del módulo
             $urlRouterProvider.otherwise("/marcosList");
-            // Definición del estado 'booksList' donde se listan los libros
-            $stateProvider.state('marcosList', {
-                // Url que aparecerá en el browser
-                url: '/marco/list',
-                 views: {
+
+            $stateProvider.state('marcos', {
+                url: '/marcos',
+                abstract: true,
+                views: {
                     'mainView': {
-                        templateUrl: basePath + 'marcos.list.html',
+                        templateUrl: basePath + 'marcos.html',
                         controller: 'marcoCtrl',
                         controllerAs: 'ctrl'
                     }
                 }
-            });
-        }
-    ]);
-})(window.angular);
+            }).state('marcosList', {
+                url: '/list',
+                parent: 'marcos',
+                views: {
+                    'listView': {
+                        templateUrl: basePath + 'marcos.list.html'
+                    }
+                }
+            }).state('marcoDetail', {
+                url: '/{marcoId:int}/detail',
+                parent: 'marcos',
+                param: {
+                    marcoId: null
+                },
+                views: {
+                    'listView': {
+                        templateUrl: basePath + 'marcos.list.html'
+                    },
+                    'detailView': {
+                        templateUrl: basePath + 'marcos.detail.html',
+                        controller: 'marcoCtrl',
+                        controllerAs: 'ctrl'
+                    }
 
+                }
+
+            });
+        }]);
+})(window.angular);
 
