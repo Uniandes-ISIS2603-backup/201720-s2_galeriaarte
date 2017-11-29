c5946190a3b856de6706cf2204a7aa2a44f00970
Kelvin <ks.estupinan@uniandes.edu.co>
2017-11-28 22:45:12 -0500

    Correcion Comentario ctrl

--- galeriaarte-web/src/main/webapp/src/modules/comentarios/new/comentarios.new.ctrl.js
+++ galeriaarte-web/src/main/webapp/src/modules/comentarios/new/comentarios.new.ctrl.js
@@ -1,22 +1,3 @@
-//(function (ng) {
-//    var mod = ng.module("comentarioModule");
-//    mod.constant("comentariosContext", "api/comentarios");
-//    mod.controller('comentarioNewCtrl', ['$scope', '$http', '$state', '$rootScope',
-//        function ($scope, $http, $state, $rootScope) {
-//            $rootScope.edit = false;
-//            $scope.createComentario = function () {
-//                $http.post('api/cliente/' + $rootScope.currentId + '/comentarios', {
-//                    id: 0,
-//                    contenido: $scope.comentarioContenido
-//                }).then(function (response) {
-//                    $state.go('comentariosList', {comentarioId: response.data.id}, {reload: true});
-//                });
-//            };
-//        }
-//    ]);
-//}
-//)(window.angular);
-
 (function (ng) {
     var mod = ng.module("comentarioModule");
     mod.constant("comentariosContext", "api/comentarios");
