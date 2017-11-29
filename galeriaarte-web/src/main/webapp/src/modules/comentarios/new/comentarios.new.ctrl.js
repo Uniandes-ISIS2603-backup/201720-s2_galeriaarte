b0344af26e916b707fe91677dc5596fdca75e6da
Kelvin <ks.estupinan@uniandes.edu.co>
2017-11-28 22:35:28 -0500

    Relacion Cliente Comentario Obra completa

--- galeriaarte-web/src/main/webapp/src/modules/comentarios/new/comentarios.new.ctrl.js
+++ galeriaarte-web/src/main/webapp/src/modules/comentarios/new/comentarios.new.ctrl.js
@@ -1,3 +1,22 @@
+//(function (ng) {
+//    var mod = ng.module("comentarioModule");
+//    mod.constant("comentariosContext", "api/comentarios");
+//    mod.controller('comentarioNewCtrl', ['$scope', '$http', '$state', '$rootScope',
+//        function ($scope, $http, $state, $rootScope) {
+//            $rootScope.edit = false;
+//            $scope.createComentario = function () {
+//                $http.post('api/cliente/' + $rootScope.currentId + '/comentarios', {
+//                    id: 0,
+//                    contenido: $scope.comentarioContenido
+//                }).then(function (response) {
+//                    $state.go('comentariosList', {comentarioId: response.data.id}, {reload: true});
+//                });
+//            };
+//        }
+//    ]);
+//}
+//)(window.angular);
+
 (function (ng) {
     var mod = ng.module("comentarioModule");
     mod.constant("comentariosContext", "api/comentarios");
@@ -5,12 +24,17 @@
         function ($scope, $http, $state, $rootScope) {
             $rootScope.edit = false;
             $scope.createComentario = function () {
-                $http.post('api/cliente/' + $rootScope.currentId + '/comentarios', {
+                
+                $http.get('api/obras/' + $state.params.obraId).then(function (responseO) {
+                $http.get('api/cliente/' + $rootScope.currentId).then(function (response) {                
+                $http.post('api/comentarios', {
                     id: 0,
-                    contenido: $scope.comentarioContenido
+                    contenido: $scope.comentarioContenido,
+                    cliente: response.data,
+                    obra: responseO.data
                 }).then(function (response) {
                     $state.go('comentariosList', {comentarioId: response.data.id}, {reload: true});
-                });
+                });});});
             };
         }
     ]);
